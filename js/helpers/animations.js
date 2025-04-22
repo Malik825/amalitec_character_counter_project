b072bbb04679c0c4b9630f474a67243e7b95d3dd
malik sule <abdulmaliksuleman75@gmail.com>
2025-04-22 01:09:18 +0000

    removed all branches

--- js/helpers/animations.js
+++
@@ -1,8 +0,0 @@
-// Simple animation for counters
-export function animateCounter(element, newValue) {
-  element.style.transform = "scale(1.1)";
-  setTimeout(() => {
-    element.textContent = newValue;
-    element.style.transform = "";
-  }, 150);
-}
