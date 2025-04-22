b34dee40028106dc61adec0580f388382a349221
malik sule <abdulmaliksuleman75@gmail.com>
2025-04-22 01:02:25 +0000

    removed all branches

--- js/module/counter.js
+++ js/module/counter.js
@@ -1,27 +1,31 @@
-import { countCharacters, countWords, countSentences } from '../helpers/counters';
-import { animateCounter } from '../helpers/animations';
+import {
+  countCharacters,
+  countWords,
+  countSentences,
+} from "../helpers/counters.js";
+import { animateCounter } from "../helpers/animations.js";
 
 export function setupCounter(elements) {
   function updateAll() {
     const text = elements.textInput.value;
     const excludeSpaces = elements.excludeSpaces.checked;
-    
+
     // Update counts
     const chars = countCharacters(text, excludeSpaces);
     const words = countWords(text);
     const sentences = countSentences(text);
-    
+
     // Display with animation
     animateCounter(elements.charCountEl, chars);
     animateCounter(elements.wordCountEl, words);
     animateCounter(elements.sentenceCountEl, sentences);
-    
+
     return { chars, words, sentences };
   }
 
   // Set up event listeners
-  elements.textInput.addEventListener('input', updateAll);
-  elements.excludeSpaces.addEventListener('change', updateAll);
+  elements.textInput.addEventListener("input", updateAll);
+  elements.excludeSpaces.addEventListener("change", updateAll);
 
   // Initial update
   updateAll();
